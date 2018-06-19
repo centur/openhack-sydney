c1928e37972de040bbc797a1f722c41c52fdfc71
Adam Murray <adam@tikabu.com.au>
2018-06-19 15:29:36 +1000

    Added model for monitor

--- challenge4/apicoreapp/apicoreapp/Controllers/ValuesController.cs
+++ challenge4/apicoreapp/apicoreapp/Controllers/ValuesController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using apicoreapp.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace apicoreapp.Controllers
 {
@@ -12,9 +15,15 @@ namespace apicoreapp.Controllers
     {
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public async Task<ActionResult<Monitor>> Get()
         {
-            return new string[] { "value1", "value2" };
+            var HttpClient = new HttpClient();
+            HttpClient.BaseAddress = new Uri("https://mcapi.us");
+
+            var result = await HttpClient.GetAsync($"server/status?ip=13.70.127.199&port=25565");
+            string resultContent = await result.Content.ReadAsStringAsync();
+            var resultJson = JsonConvert.DeserializeObject<Monitor>(resultContent);
+            return resultJson;
         }
 
         // GET api/values/5
---
+++ challenge4/apicoreapp/apicoreapp/Models/Monitor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apicoreapp.Models
+{
+    public class Monitor
+    {
+        public bool Online { get; set; }
+        public string Motd { get; set; }
+        public string Error { get; set; }
+        public Players Players { get; set; }
+        
+
+
+
+
+        /* 
+         status       : success
+online       : True
+motd         : A Minecraft Server Powered by Docker
+error        :
+players      : @{max=20; now=0}
+server       : @{name=1.12.2; protocol=340}
+last_online  : 1529384898
+last_updated : 1529384898
+duration     : 436821122 
+         */
+    }
+
+    public class Players
+    {
+        public int Max { get; set; }
+        public int Now { get; set; }
+
+    }
+}
