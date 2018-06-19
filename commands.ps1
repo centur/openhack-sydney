da8cc54f1720e06b544e6fdff8dc8e85ce74399d
Alexey Shcherbak <alexey@drawboard.com>
2018-06-19 11:34:08 +1000

    attempt to  start cluster with rbac and monitoring.

--- commands.ps1
+++ commands.ps1
@@ -1,19 +1,23 @@
+$kubeGroup = "hack9-kube"
+$kubeName = "hack9cluster"
+
+az aks browse --resource-group $kubeGroup --name $kubeName
+
 az login -u hacker5@OTAPRD485ops.onmicrosoft.com
 
-az aks get-credentials --resource-group hack9-kube --name hack9cluster
+az aks get-credentials --resource-group $kubeGroup --name $kubeName
+kubectl config current-context # give current details where kubectl sends commands
+kubectl config get-contexts 
 
 ## Challenge 2 
-Build up yaml file and call:
+# Build up yaml file and call:
 kubectl apply -f hack9.yaml
 
 
 ## Challenge 3
-
 kubectl get storageclass
 
 kubectl apply -f azure-premium.yaml
-kubectl config current-context
-kubectl config get-contexts
+kubectl get pods --all-namespaces
 
-
-docker pull mcr.microsoft.com/powershell - powershell core
+#docker pull mcr.microsoft.com/powershell - powershell core
