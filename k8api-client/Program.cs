5bd967d98a986b98e4cada1a2ff126f7d813ee59
Alexey Shcherbak <alexey@drawboard.com>
2018-06-19 14:24:56 +1000

    initial app to experiment with api

---
+++ k8api-client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace k8api_client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
