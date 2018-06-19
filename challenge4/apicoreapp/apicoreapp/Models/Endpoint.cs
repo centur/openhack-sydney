cd070da9234d89f28442b999a9aaad4aee4036f3
alexander grigorev <a.grigorev@live.com>
2018-06-19 15:34:41 +1000

    added Tenant model

---
+++ challenge4/apicoreapp/apicoreapp/Models/Endpoint.cs
@@ -0,0 +1,8 @@
+﻿namespace apicoreapp.Model
+{
+    public class Endpoint
+    {
+        public string Minecraft { get; set; }
+        public string Rcon { get; set; } 
+    }
+}
---
+++ challenge4/apicoreapp/apicoreapp/Models/Tenant.cs
@@ -0,0 +1,8 @@
+﻿namespace apicoreapp.Model
+{
+    public class Tenant
+    {
+        public string Name { get; set; }
+        public Endpoint[] Endpoints { get; set; }    
+    }
+}
